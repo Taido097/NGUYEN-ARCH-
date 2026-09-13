a588fdbc3c7c00efe677a86a87af6996ed1dcb18
Taido097 <taido097@gmail.com>
2026-09-12 20:15:43 -0700

    Match project type dropdown to provided services

--- app/client-demos/client-8889/residential/contact/page.tsx
+++ app/client-demos/client-8889/residential/contact/page.tsx
@@ -110,11 +110,12 @@ export default function ContactPage() {
                       <option value="">Select a project type…</option>
                       <option value="Custom Home">Custom Home</option>
                       <option value="Addition / Remodel">Addition / Remodel</option>
-                      <option value="ADU">ADU (Accessory Dwelling Unit)</option>
+                      <option value="ADU & SB9">ADU &amp; SB9 (Accessory Dwelling Unit)</option>
                       <option value="Multifamily">Multifamily</option>
                       <option value="Commercial">Commercial</option>
                       <option value="Land Development">Land Development</option>
                       <option value="Engineering / Approvals">Engineering &amp; Approvals</option>
+                      <option value="Builders Complete Delivery">Builders Complete Delivery</option>
                       <option value="Other">Other</option>
                     </select>
                   </label>
